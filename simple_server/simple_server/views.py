96c91a8415c0e18a97976da2e8f3034f6eb7c047
LeeThomas13 <lmtthomasritz@gmail.com>
2021-01-20 16:34:05 -0800

    added simple server and temp csv to backend

--- simple_server/simple_server/views.py
+++ simple_server/simple_server/views.py
@@ -4,5 +4,5 @@ from .tweet_dumper import get_all_tweets
 #next step is to replace culk with data from a post
 #request.POST['name_of_user']
 def data_view(request):
-    temp = get_all_tweets("IncredibleCulk")
+    temp = get_all_tweets("Mr2dayman")
     return JsonResponse({'lines':temp})
