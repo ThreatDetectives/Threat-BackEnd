e181ddffff0644e4edd5cfe0a419745fbeb67f03
TAYLOR JOHNSON <TAYLOR.MCMINN@GMAIL.COM>
2021-01-21 17:53:37 -0800

    cors whitelist vercel

--- simple_server/simple_server/settings.py
+++ simple_server/simple_server/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-#some change
+# some change
 
 from pathlib import Path
 import environ
@@ -32,12 +32,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str('SECRET_KEY')
+SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG')
+DEBUG = env.bool("DEBUG")
 
-ALLOWED_HOSTS = tuple(env.list('ALLOWED_HOSTS'))
+ALLOWED_HOSTS = tuple(env.list("ALLOWED_HOSTS"))
 
 
 # Application definition
@@ -135,4 +135,6 @@ STATIC_URL = "/static/"
 # CORS_ALLOW_ALL_ORIGINS = True
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000",
+    "https://threatdetectives-f8j9zpof5.vercel.app/",
+    "https://threat-detector-git-troubleshooting.threatdetectives.vercel.app/",
 ]
