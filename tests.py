6f26098dd055ed9b76722676096d201fa5cae60e
Hexx King <hexxking13@gmail.com>
2021-01-22 10:33:28 -0800

    happy path tests written for model class in views.py

--- tests.py
+++
@@ -1,9 +0,0 @@
-from nlp_model.model import NLPModel
-import pytest
-
-def test_remove_RT_user():
-    nlp = NLPModel()
-    text = '#vapesquad #fatclouds @mickRipsFat siiiick clouds bruv saw you at walmart chuckin em'
-    actual = nlp.remove_RT_user(text)
-    expected = 'siiiick clouds bruv saw you at walmart chuckin em'
-    assert actual == expected
