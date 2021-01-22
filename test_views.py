df80c8b21442fdd668439d822550c88e7f875573
LeeThomas13 <lmtthomasritz@gmail.com>
2021-01-22 11:28:04 -0800

    added 10 tests, all passing

--- test_views.py
+++ test_views.py
@@ -26,3 +26,45 @@ def test_tokenize():
     expected = ['Tokenize', 'this']
     assert actual == expected
 
+def test_edge_case():
+    nlp = NLPModel()
+    text = '@11111'
+    actual = nlp.remove_RT_user(text)
+    expected = ''
+    assert actual == expected
+
+def test_edge_case_two():
+    nlp = NLPModel()
+    text = 'that chick said "the" fucks goin on'
+    actual = nlp.remove_punctuation(text)
+    expected = 'that chick said the fucks goin on'
+    assert actual == expected
+
+def test_all_removes():
+    nlp = NLPModel()
+    text = '#vapesquad "#fatclouds" @mickRipsFat siiiick clouds bruv saw you at walmart /?/?/?/ :) :( >xD chuckin em'
+    actual = nlp.remove_RT_user(text)
+    actual = nlp.remove_punctuation(text)
+    actual = nlp.tokenize(text)
+    expected = ['', 'vapesquad', 'fatclouds', 'mickRipsFat', 'siiiick', 'clouds', 'bruv', 'saw', 'you', 'at', 'walmart', 'xD', 'chuckin', 'em']
+    assert actual == expected
+
+
+def test_expected_failure():
+    nlp = NLPModel()
+    text = 5
+    with pytest.raises(TypeError):
+        nlp.remove_RT_user(text)
+
+def test_expected_failure_two():
+    nlp = NLPModel()
+    text = 5
+    with pytest.raises(TypeError):
+        nlp.tokenize(text)
+
+def test_expected_failure_three():
+    nlp = NLPModel()
+    text = 5
+    with pytest.raises(TypeError):
+        nlp.remove_punctuation(text)
+
